432d47e8070f89ae21f423b811ba0253295e6241
glyn-stevens <glynst@gmail.com>
2024-02-09 21:48:42 +0000

    improve lint, add mypy

--- bin/lint.ps1
+++ bin/lint.ps1
@@ -1,8 +1,13 @@
+$TARGER_DIR="medicine"
+
 "`nisort:"
-poetry run isort .
+poetry run isort $TARGER_DIR
 
 "`nblack:"
-poetry run black -t py310 -l 79 medicine
+poetry run black -t py310 -l 99 $TARGER_DIR
 
 "`nflake8:"
-poetry run flake8 . --max-line-length=99
+poetry run flake8 $TARGER_DIR --max-line-length=99
+
+"`nmypy tests:"
+poetry run mypy $TARGER_DIR --ignore-missing-imports --check-untyped-defs
