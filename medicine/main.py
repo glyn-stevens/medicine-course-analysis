fbcf79a6a32a93b8a58a937462b931bb59cd94ae
glyn-stevens <glynst@gmail.com>
2024-02-10 00:25:40 +0000

    - convert area details to dataclass - store details about areas interested in as globals - add stat for uncommutable locations which have rotation_in_main_city

--- medicine/main.py
+++ medicine/main.py
@@ -1,6 +1,57 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import pandas as pd
+
+@dataclass
+class AreaDetails:
+    region: str
+    main_city: list[str]
+    commutatble_cities: list[str]
+    
+
+CAMBRIDGE = AreaDetails(
+    region= "East of England Foundation School",
+    main_city= [
+        "Cambridge",
+        "Papworth Everard",
+        "Papworth",
+        "Papworth Hospital",
+    ],
+    commutatble_cities= [
+        "Bury St Edmunds",
+        "Harlow",
+        "Huntingdon",
+        "King's Lynn",
+        "Stevenage",
+        "CPFT",
+        "Cambridgeshire and Peterborough",
+    ],
+)
+SHEFFIELD = AreaDetails(
+    region= "Yorkshire and Humber Foundation School",
+    main_city= [
+        "Sheffield S10 2JF",
+        "Sheffield S10 2SF",
+        "Sheffield S10 2SJ",
+        "Sheffield S10 2TH",
+        "Sheffield S11 9BF",
+        "Sheffield S11 9NE",
+        "Sheffield S12 4QN",
+        "Sheffield S2 3QE",
+        "Sheffield S26 4TH",
+        "Sheffield S35 8QS",
+        "Sheffield S5 7AU",
+        "Sheffield S5 7JT",
+    ],
+    commutatble_cities=["Rotherham S60 2UD"],
+)
+
+SOUTH_COAST = AreaDetails(
+    region="Wessex Foundation School",
+    main_city=["Poole", "Bournemouth"],
+    commutatble_cities=[]
+)
 
 DATA_DIR = Path.cwd() / "data"
 
@@ -10,67 +61,57 @@ def rotation_col(rotation_num: int) -> str:
 
 
 def main(
-    region: str,
-    main_city: str,
     program_type: str,
-    commutatble_cities: list[str],
+    area: AreaDetails,
+    print_matching_placements: bool
 ) -> None:
     # Column headers
     region_col = "Foundation School"
     program_type_col = "Programme Type"
     all_rotation_cols = [rotation_col(x) for x in [1, 2, 3, 4, 5, 6]]
-    
+
     # Read data
     file_path = DATA_DIR / "foundation_programmes.csv"
     df_all = pd.read_csv(file_path)
-    
+
     # Masks and initial filter
-    mask_region = (df_all[region_col] == region)
-    mask_program_type = (df_all[program_type_col] == program_type)
+    mask_region = df_all[region_col] == area.region
+    mask_program_type = df_all[program_type_col] == program_type
     df_region = df_all[mask_region & mask_program_type]
-    mask_main_city = (df_region[rotation_col(1)] == main_city) | (
-        df_region[rotation_col(4)] == main_city
+    mask_main_city = (df_region[rotation_col(1)].isin(area.main_city)) | (
+        df_region[rotation_col(4)].isin(area.main_city)
     )
-    commutatble_cities.append(main_city)
-    mask_commutable = df_region[all_rotation_cols].isin(commutatble_cities).sum(axis=1) >= 5
-    mask_comutable_with_main_city = (mask_main_city & mask_commutable)
-    
+    main_city_or_commutable = area.commutatble_cities + area.main_city
+    mask_commutable = df_region[all_rotation_cols].isin(main_city_or_commutable).sum(axis=1) >= 5
+    mask_comutable_with_main_city = mask_main_city & mask_commutable
+
     # Calculate stats
     region_stats = {}
-    placements_in_region = len(df_region)
-    region_stats[f"Region: {region}"] = placements_in_region
-    region_stats[f"Rotation 1 or 4 in: {main_city}"] = mask_main_city.sum()
-    region_stats[f"Rotation 1 or 4 in: {main_city}, and at least 5/6 rotations commutable"] = (
+    num_in_region = len(df_region)
+    region_stats[f"Region: {area.region}"] = num_in_region
+    region_stats[f"Rotation 1 or 4 in: {area.main_city[0]}"] = mask_main_city.sum()
+    region_stats[f"Rotation 1 or 4 in: {area.main_city[0]}, and at least 5/6 rotations commutable"] = (
         mask_comutable_with_main_city.sum()
     )
-    region_stats[f"At least 5/6 rotations commutable from: {main_city}"] = mask_commutable.sum()
-    all_cities_in_region = pd.concat([df_region[col] for col in all_rotation_cols])
-    uncommutable_cities = {city for city in all_cities_in_region if city not in commutatble_cities}
+    region_stats[f"At least 5/6 rotations commutable from: {area.main_city[0]}"] = mask_commutable.sum()
+    all_locations_in_region = pd.concat([df_region[col] for col in all_rotation_cols])
+    uncommutable_cities = {loc for loc in all_locations_in_region if loc not in main_city_or_commutable}
+    locations_with_rotation_in_main_city = pd.concat([df_region[mask_main_city][col] for col in all_rotation_cols])
+    uncommutable_with_rotation_in_main_city = {loc for loc in locations_with_rotation_in_main_city if loc not in main_city_or_commutable}
     
     # Print results
     print(f"Results for program type: {program_type}")
     for key, stat in region_stats.items():
-        print(f"Number placements with {key}: {stat} ( = {stat/placements_in_region*100:.1f}%)")
-    print(f"Cities deemed uncommutable: {uncommutable_cities}")
-    print("Placement locations:")
-    pd.set_option('display.max_rows', 200)
-    print(df_region[mask_commutable][all_rotation_cols])
+        print(f"Number placements with {key}: {stat} ( = {stat/num_in_region*100:.1f}%)")
+    print(f"Locations deemed uncommutable: {sorted(uncommutable_cities)}")
+    print(f"Locations deemed uncommutable which has a rotation in which also have a rotation in main city: {uncommutable_with_rotation_in_main_city}")
+    if print_matching_placements:
+        print("Placement locations:")
+        pd.set_option("display.max_rows", 200)
+        print(df_region[mask_commutable][all_rotation_cols])
 
 
 if __name__ == "__main__":
     program_type = "FP"
-    region = "East of England Foundation School"
-    main_city = "Cambridge"
-    comutatble_cities = [
-        "Bury St Edmunds",
-        "Harlow",
-        "Huntingdon",
-        "King's Lynn",
-        "Papworth Everard",
-        "Papworth",
-        "Papworth Hospital",
-        "Stevenage",
-        "CPFT",
-        "Cambridgeshire and Peterborough"
-    ]
-    main(region, main_city, program_type, comutatble_cities)
+    area_to_investigate = SOUTH_COAST
+    main(program_type, area_to_investigate, print_matching_placements=False)
